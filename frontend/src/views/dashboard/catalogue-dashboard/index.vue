23592fb36d7e0b17f4ec4378ca1a5230c2103779
LinkunGao <gaolinkun123@gmail.com>
2025-06-10 04:05:15 +1200

    reuse catalogue view header

--- frontend/src/views/dashboard/catalogue-dashboard/index.vue
+++ frontend/src/views/dashboard/catalogue-dashboard/index.vue
@@ -4,37 +4,13 @@
             <h1 class="mx-auto">Catalogue Dashboard</h1>
         </NavHome>
         <div class="main-container overflow-y-auto d-flex align-center">
-            <v-row justify="center">
-                <v-col
-                    cols="12"
-                    md="6"
-                >
-                    <SimpleInfoCard
-                        category="Seek"
-                        herf="http://130.216.217.147:3000/"
-                        title="Edit in Seek website"
-                        description="Programmes, Projects, Investigations, Studies, Assays, Measurements, Models"
-                    />
-                </v-col>
-                <v-col
-                    cols="12"
-                    md="6"
-                >
-                    <SimpleInfoCard
-                        category="Catalogue dashboard"
-                        herf="Home"
-                        title="Edit in Catalogue dashboard"
-                        description="Workflows, Tools​"
-                    />
-                </v-col>
-            </v-row>
+            <router-view/>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import NavHome from '@/views/dashboard/components/NavHome.vue';
-import SimpleInfoCard from '@/views/dashboard/components/SimpleInfoCard.vue';
 
 </script>
 
