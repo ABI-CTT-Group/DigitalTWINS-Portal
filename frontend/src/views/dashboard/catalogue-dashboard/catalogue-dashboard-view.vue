9ff1da1d00d0a319568dfbac3dc57ce2d721900c
LinkunGao <gaolinkun123@gmail.com>
2025-06-10 04:02:00 +1200

    add catalogue dashboard view

---
+++ frontend/src/views/dashboard/catalogue-dashboard/catalogue-dashboard-view.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-row justify="center">
+        <v-col
+            cols="12"
+            md="6"
+        >
+            <SimpleInfoCard
+                category="Seek"
+                herf="http://130.216.217.147:3000/"
+                title="Edit in Seek website"
+                description="Programmes, Projects, Investigations, Studies, Assays, Measurements, Models"
+            />
+        </v-col>
+        <v-col
+            cols="12"
+            md="6"
+        >
+            <SimpleInfoCard
+                category="Catalogue dashboard"
+                herf="WorkflowToolsViewer"
+                title="Edit in Catalogue dashboard"
+                description="Workflows, Tools​"
+            />
+        </v-col>
+    </v-row>
+    
+</template>
+
+<script setup lang="ts">
+import SimpleInfoCard from '@/views/dashboard/components/SimpleInfoCard.vue';
+</script>
+
+<style scoped>
+
+</style>
