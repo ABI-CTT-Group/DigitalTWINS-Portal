760033c96bcec97c20da7cfc56b4a95d620a6be7
LinkunGao <gaolinkun123@gmail.com>
2025-09-13 00:47:36 +1200

    Update backend with plugin build

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,17 +5,26 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import Response
 from pathlib import Path
 import io
-from router import tumour_position, dashboard, clinical_report_viewer, tumour_segmentation
+from router import dashboard, clinical_report_viewer
 from utils import Config
+from contextlib import asynccontextmanager
 
-app = FastAPI()
 
-app.include_router(tumour_position.router)
-app.include_router(tumour_segmentation.router)
+# When fastapi start will execute this function first
+# yield will pause the code, when fastapi stop will execute the remain codes
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
+    print("starting lifespan")
+    yield
+    # Shutdown
+    print("ending lifespan")
+    pass
+
+
+app = FastAPI(title="DigitalTWINS Portal API", verison="1.0.0", lifespan=lifespan)
 app.include_router(dashboard.router)
 app.include_router(clinical_report_viewer.router)
-
-expose_headers = ["x-volume", "x-file-name", "Content-Disposition"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -23,24 +32,17 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=expose_headers
 )
-
-
-# @app.on_event("startup")
-# async def startup_event():
 
 
 @app.get('/')
 async def root():
     print(Config.BASE_PATH)
     current_path = Path.cwd()
-    print(current_path)
-
     # Get the directory of the current script
     script_path = Path(__file__).resolve().parent
     print(script_path)
-    return "Welcome to segmentation backend"
+    return {"message": "DigitalTWINS Portal API", "version": "1.0.0"}
 
 
 @app.get("/api/test")
@@ -59,8 +61,6 @@ async def test():
     response.headers["x-file-name"] = "This is a custom string."
 
     return response
-
-
 
 
 if __name__ == '__main__':
